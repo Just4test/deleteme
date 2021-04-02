bca2ea250820db3dd85cce3095180104b193de19
羽 <Just4test@users.noreply.github.com>
2021-04-02 17:46:43 +0800

    Create hello.ts

---
+++ hello.ts
@@ -0,0 +1,52 @@
+import { readLines } from "https://deno.land/std/io/mod.ts";
+import { parse } from "https://deno.land/std/encoding/yaml.ts";
+
+function modify(url: URL):string
+{
+  let source = (await (await fetch((url.pathname+fetchURL.search).slice(1), 
+    {headers:{'User-Agent':'Surge'}})).text()).split('\n')
+  
+  let result: string[] = ['#!MANAGED-CONFIG '+url+' interval=3600 strict=true'];
+  let areas: {[key:string]: string[]} = {};
+  let area = '';
+  areas[area] = []
+  
+  for(var i of source){
+    i = i.trim()
+    if(i.length > 2 && i[0] == '[' && i[i.length -1] == ']'){
+      area = i
+      areas[area] = []
+    }else{
+      areas[area].push(i)
+    }
+  }
+  delete areas['']
+  
+  
+  let modify = parse(Deno.readTextFileSync('modify.yaml')) as any
+  
+  let m = modify['push'] as {[key:string]: string[]}
+  for(var i in m){
+    areas['[' + i + ']'] = areas['[' + i + ']'].concat(m[i])
+  }
+  
+  m = modify['unshift'] as {[key:string]: string[]}
+  for(var i in m){
+    areas['[' + i + ']'] = m[i].concat(areas['[' + i + ']'])
+  }
+  
+  for(var i in areas){
+    result.push(i)
+    result = result.concat(areas[i])
+  }
+  return result.join('\n')
+  
+}
+
+addEventListener("fetch", (event) => {
+  event.respondWith(new Response(modify(new URL(event.request.url)), {
+      headers: {
+        "content-type": "text/plain; charset=UTF-8",
+      },
+   }))
+});
