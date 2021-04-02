b26087ab1a3f8b1a014d00a43afa449d2c97d161
羽 <Just4test@users.noreply.github.com>
2021-04-02 17:27:01 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,8 +1,8 @@
 
 
 addEventListener("fetch", (event) => {
-  console.log(event.request.url)
-  console.log(event.request.path)
+  console.log((new URL(event.request.url)).path)
+  console.log((new URL(event.request.url)).hash)
   event.respondWith(new Response('111111111111', {
       headers: {
         "content-type": "application/json; charset=UTF-8",
